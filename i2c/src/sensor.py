ae2af11223081101440ea46266bc98cfa4e1465c
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-02-18 11:50:29 +0000

    Add files via upload

--- i2c/src/sensor.py
+++ i2c/src/sensor.py
@@ -7,4 +7,9 @@
 # the Adafruit Triple Axis ADXL345 breakout board:
 # http://shop.pimoroni.com/products/adafruit-triple-axis-accelerometer
 
+import I2C_LCD_driver
+from time import *
 
+mylcd = I2C_LCD_driver.lcd()
+
+mylcd.lcd_display_string("Hello World!", 1)
